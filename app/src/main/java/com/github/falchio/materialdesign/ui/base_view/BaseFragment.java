6abdd0b77113e4eeae6e086559d4ca8a8feb023c
Falchio <53504233+Falchio@users.noreply.github.com>
2020-08-05 21:42:38 +0300

    fix FAB Behavior

--- app/src/main/java/com/github/falchio/materialdesign/ui/base_view/BaseFragment.java
+++ app/src/main/java/com/github/falchio/materialdesign/ui/base_view/BaseFragment.java
@@ -1,10 +1,13 @@
 package com.github.falchio.materialdesign.ui.base_view;
 
+import android.view.animation.LinearInterpolator;
+
 import androidx.fragment.app.Fragment;
 
 import com.github.falchio.materialdesign.R;
 import com.google.android.material.appbar.AppBarLayout;
 import com.google.android.material.appbar.CollapsingToolbarLayout;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 abstract public class BaseFragment extends Fragment {
     private String title;
@@ -16,5 +19,7 @@ abstract public class BaseFragment extends Fragment {
         collapsingToolbarLayout.setTitle(title);
         AppBarLayout appBarLayout = requireActivity().findViewById(R.id.app_bar_layout);
         appBarLayout.setExpanded(true);
+        FloatingActionButton fab = requireActivity().findViewById(R.id.fab);
+        fab.animate().translationY(0).setInterpolator(new LinearInterpolator()).start();
     }
 }
