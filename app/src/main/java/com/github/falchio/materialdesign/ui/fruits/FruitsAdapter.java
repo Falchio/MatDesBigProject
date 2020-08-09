e277b03ad0800ca86f89fa6530b7ff9401635af5
Falchio <53504233+Falchio@users.noreply.github.com>
2020-08-09 21:49:24 +0300

    Add RecyclerView with cards

---
+++ app/src/main/java/com/github/falchio/materialdesign/ui/fruits/FruitsAdapter.java
@@ -0,0 +1,64 @@
+package com.github.falchio.materialdesign.ui.fruits;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.github.falchio.materialdesign.R;
+
+import java.util.List;
+
+public class FruitsAdapter extends RecyclerView.Adapter<FruitsAdapter.CardFruitsViewHolder> {
+    private List<CardFruits> listCard;
+
+    public FruitsAdapter(List<CardFruits> listCard) {
+        this.listCard = listCard;
+    }
+
+    @NonNull
+    @Override
+    public CardFruitsViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.fruits_item, parent, false);
+        return new CardFruitsViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull CardFruitsViewHolder holder, int position) {
+        CardFruits currentCard = listCard.get(position);
+
+        holder.title.setText(currentCard.getTitle());
+        holder.description.setText(currentCard.getDescription());
+        holder.imageView.setImageResource(currentCard.getImageID());
+
+        View.OnClickListener onClickListener = v -> Toast.makeText(v.getContext(), "You clicked on card #" + (position+1), Toast.LENGTH_SHORT).show();
+
+        holder.imageView.setOnClickListener(onClickListener);
+        holder.title.setOnClickListener(onClickListener);
+        holder.description.setOnClickListener(onClickListener);
+    }
+
+    @Override
+    public int getItemCount() {
+        return listCard.size();
+    }
+
+
+    public static class CardFruitsViewHolder extends RecyclerView.ViewHolder {
+        private ImageView imageView;
+        private TextView title;
+        private TextView description;
+
+        public CardFruitsViewHolder(@NonNull View itemView) {
+            super(itemView);
+            imageView = itemView.findViewById(R.id.cardview_image);
+            title = itemView.findViewById(R.id.cardview_title);
+            description = itemView.findViewById(R.id.cardview_description);
+        }
+    }
+}
--- app/src/main/java/com/github/falchio/materialdesign/ui/fruits/FruitsFragment.java
+++ app/src/main/java/com/github/falchio/materialdesign/ui/fruits/FruitsFragment.java
@@ -4,17 +4,46 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.github.falchio.materialdesign.R;
 import com.github.falchio.materialdesign.ui.base_view.BaseFragment;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class FruitsFragment extends BaseFragment {
-    private String title="Fruits";
+    private String title = "Fruits";
 
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_fruits, container, false);
+        View root = inflater.inflate(R.layout.fragment_fruits, container, false);
+
+        List<CardFruits> fruitsList = new ArrayList<>();
+        int imageId;
+
+        for (int i =1; i<=20; i++){
+            if (i%2==0){
+                imageId = R.drawable.fruits;
+            }else {
+                imageId = R.drawable.nature;
+            }
+            fruitsList.add(new CardFruits("Title: fruit #" + i, "Description: description #" + i, imageId));
+        }
+
+        RecyclerView rv = root.findViewById(R.id.recycler);
+        if (rv != null) {
+            rv.setHasFixedSize(true);
+            LinearLayoutManager layoutManager = new LinearLayoutManager(getContext());
+            layoutManager.setOrientation(LinearLayoutManager.VERTICAL);
+            rv.setLayoutManager(layoutManager);
+            FruitsAdapter adapter = new FruitsAdapter(fruitsList);
+            rv.setAdapter(adapter);
+        }
+        return root;
     }
 }
